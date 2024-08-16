b1be3a8aaa59e7d166c63a13f6b460e50e21ff5a
Phalanndwa Tendani <2023393305@ufs4life.ac.za>
2024-08-16 12:27:53 +0200

    Add files via upload
    This is an app created using windows forms app, you can use this app to store Student marks on a txt file in the location of your choice

---
+++ StudentsFinal/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace StudentsFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(454, 120);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(185, 38);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(454, 184);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(185, 41);
+            this.btnSaveFile.TabIndex = 1;
+            this.btnSaveFile.Text = "Save To File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(454, 247);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(185, 43);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Read From File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(454, 306);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(185, 40);
+            this.btnDisplay.TabIndex = 3;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(454, 365);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(185, 49);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtNames
+            // 
+            this.txtNames.Location = new System.Drawing.Point(454, 69);
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(126, 20);
+            this.txtNames.TabIndex = 5;
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(586, 69);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(53, 20);
+            this.txtMark.TabIndex = 6;
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(51, 69);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(169, 355);
+            this.lstStudents.TabIndex = 7;
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(275, 69);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(120, 355);
+            this.lstMarks.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Students";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Marks";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Student";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(586, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Mark";
+            // 
+            // dlgOpenFile
+            // 
+            this.dlgOpenFile.FileName = "dlgOpenFile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+    }
+}
+
---
+++ StudentsFinal/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsFinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lstMarks.Items.Add(txtMark.Text);
+            lstStudents.Items.Add(txtNames.Text);
+            txtMark.Clear();
+            txtNames.Clear();
+            txtNames.Focus();
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            dlgSaveFile.InitialDirectory = Application.StartupPath;
+            dlgSaveFile.FileName = "Students.txt";
+            dlgSaveFile.OverwritePrompt = false;
+
+            if (dlgSaveFile.ShowDialog()==DialogResult.OK)
+            {
+                using (StreamWriter f = new StreamWriter(dlgSaveFile.FileName))
+                {
+                    for (int i = 0; i < lstStudents.Items.Count; i++)
+                    {
+                        f.Write(lstStudents.Items[i] +"\t");
+
+                        if (i<lstMarks.Items.Count)
+                        {
+                            f.Write(lstMarks.Items[i] + "\n");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            dlgOpenFile.InitialDirectory = Application.StartupPath;
+            dlgOpenFile.FileName = "Students.txt";
+
+            using(StreamReader f = new StreamReader(dlgOpenFile.FileName))
+            {
+                string sLine = "";
+                string[] sFields;
+                while (!f.EndOfStream)
+                {
+                    sLine = f.ReadLine();
+                    sFields = sLine.Split('\t');
+                    lstStudents.Items.Add(sFields[0]);
+                    lstMarks.Items.Add(sFields[1]);
+           
+                }
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            string sResults="";
+
+            for (int i = 0; i < lstStudents.Items.Count; i++)
+            {
+                sResults += lstStudents.Items[i] +"\t" + lstMarks.Items[i] +"\n";
+            }
+            MessageBox.Show(sResults, "Results", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
